a8127198eed26746fdfe994f8504ec5881868151
Aquiles0903 <dilver.2306@gmail.com>
2026-05-23 18:13:01 -0600

    Avance del proyecto.

--- GestionEmpleados/src/Nomina.java
+++ GestionEmpleados/src/Nomina.java
@@ -1,7 +1,45 @@
 import java.util.List;
 import java.util.ArrayList;
-
+import java.time.LocalDate;
 public class Nomina {
+    private List<Empleado> empleados;
+    private LocalDate periodoPago;
+    private double nominaTotal;
+
+public Nomina(LocalDate periodoPago, double nominaTotal){
+    this.empleados = new ArrayList<>();
+    this.periodoPago = periodoPago;
+    this.nominaTotal = 0.0;
+}
+// Manejo de Empleados
+public void agregarEmpleado(Empleado empleado){
+    this.empleados.add(empleado);
+}
+/* Getters y Setters */
+
+public List<Empleado> getEmpleados() {
+    return empleados;
+}
+public void setEmpleados(List<Empleado> empleados) {
+    this.empleados = empleados;
+}
+public LocalDate getPeriodoPago() {
+    return periodoPago;
+}
+public void setPeriodoPago(LocalDate periodoPago) {
+    this.periodoPago = periodoPago;
+}
+public double getNominaTotal() {
+    return nominaTotal;
+}
+public void setNominaTotal(double nominaTotal) {
+    this.nominaTotal = nominaTotal;
+}
+
+
+
+
+
 
     
 }
